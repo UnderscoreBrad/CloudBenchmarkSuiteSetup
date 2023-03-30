1131c44c2a4a422a06f55652ba795183b678201f
vboxuser <vboxuser@Ubuntu22.ubuntu22.virtualbox.org>
2023-03-30 16:20:49 -0400

    moved setup.sh to main dir

--- setup.sh
+++ setup.sh
@@ -11,20 +11,20 @@ read ans
 
 startDir=$PWD
 
-chmod a+x setup_apt.sh
-chmod a+x setup_docker.sh
-chmod a+x setup_git.sh
-chmod a+x setup_go.sh
-chmod a+x setup_postgres.sh
-chmod a+x setup_webserv.sh 
+chmod a+x setup/setup_apt.sh
+chmod a+x setup/setup_docker.sh
+chmod a+x setup/setup_git.sh
+chmod a+x setup/setup_go.sh
+chmod a+x setup/setup_postgres.sh
+chmod a+x setup/setup_webserv.sh 
 
 # webserver is not run until needed
 
-./setup_apt.sh
-./setup_docker.sh
-./setup_git.sh
-./setup_go.sh
-./setup_postgres.sh
+setup/./setup_apt.sh
+setup/./setup_docker.sh
+setup/./setup_git.sh
+setup/./setup_go.sh
+setup/./setup_postgres.sh
 
 npm install autocannon -g
 

6151276309f98a6cc12e29a72a5d9c1a0737dca7
UnderscoreBrad <bradleyestus@gmail.com>
2023-03-31 18:27:24 +0000

    pgbench script finished?

--- run/run_pgbench.sh
+++ run/run_pgbench.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 cores=$(getconf _NPROCESSORS_ONLN)
-transactions=$((1000000/$cores))
-echo $transactions
+transactionsA=$((100000/$cores))
+transactionsB=$((1000000/$cores))
 #transactions= 10000000/$cores
 # Run Database Benchmarks
 # the default DB size is ~16MB, scale to 3.2GB.
@@ -11,5 +11,13 @@ echo $transactions
 # -s: DB scale factor
 mkdir pgbench
 chmod 777 pgbench
+echo "test type: small (scale 50, 2 threads, 10000 transactions/thread)"
+sudo -u postgres pgbench -i -s 50 example
+sudo -u postgres pgbench -c 10 -j 2 -t 10000 example
+echo "test type: medium (scale 200, max threads, 100000 transactions total)"
 sudo -u postgres pgbench -i -s 200 example
-sudo -u postgres pgbench -n -c 10 -s 200 --jobs=${cores} --transactions=${transactions} example
+sudo -u postgres pgbench -c 10 --jobs=${cores} --transactions=${transactionsA} example
+# echo "test type: LARGE (scale 200, max threads, 1000000 transactions total)"
+# sudo -u postgres pgbench -i -s 200 example
+# sudo -u postgres pgbench -c 10 --jobs=${cores} --transactions=${transactionsB} example
+# echo "tests completed."

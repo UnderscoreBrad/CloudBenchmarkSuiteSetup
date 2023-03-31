79d9081e0e5254b831be88bcd7aea9b0a5617a71
Brad Estus <50963179+UnderscoreBrad@users.noreply.github.com>
2023-03-31 13:43:12 -0400

    Update run_pgbench.sh

--- run/run_pgbench.sh
+++ run/run_pgbench.sh
@@ -5,13 +5,13 @@ transactions=$((1000000/$cores))
 echo $transactions
 #transactions= 10000000/$cores
 # Run Database Benchmarks
-# the default DB size is ~16MB, scale that to 800MB, 1.6GB, and 3.2GB.
+# the default DB size is ~16MB, scale to 3.2GB.
 # -i: initialize tables
 # -c: specify number of clients
 # -s: DB scale factor
 mkdir pgbench
 chmod 777 pgbench
-sudo -u postgres pgbench -i -s 50 example
-#sudo -u postgres pgbench -c 10 -s 50 --jobs=${cores} --transactions=${transactions} example
-#sudo -u postgres pgbench -c 10 -s 100 --jobs=${cores} --transactions=${transactions} example
-#sudo -u postgres pgbench -c 10 -s 200 --jobs=${cores} --transactions=${transactions} example
+sudo -u postgres pgbench -i -s 200 example
+sudo -u postgres pgbench -c 10 -s 200 --jobs=${cores} --transactions=${transactions} example
+sudo -u postgres pgbench -c 10 -s 200 --jobs=${cores} --transactions=${transactions} example
+sudo -u postgres pgbench -c 10 -s 200 --jobs=${cores} --transactions=${transactions} example

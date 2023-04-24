313f6c83b7572018e23d42982ffee7f63092b079
Brad Estus <50963179+UnderscoreBrad@users.noreply.github.com>
2023-04-24 17:27:03 -0400

    Update runycsb.sh

--- runycsb.sh
+++ runycsb.sh
@@ -1,4 +1,5 @@
   #!/bin/bash
   echo "make sure to run mongod in a separate terminal window before running this script."
+  cd ycsb-mongodb-binding-0.15.0
   ./bin/ycsb load mongodb -s -P workloads/workloada -p recordcount=500000 -threads 16 -p mongodb.url="mongodb://localhost:27017/admin"
   ./bin/ycsb run mongodb -s -P workloads/workloada -p mongodb.url="mongodb://localhost:27017/admin" -p operationcount=5000000 -threads 16
